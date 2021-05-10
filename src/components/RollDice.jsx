b7ba58de6410ff048b6678f2ef94da0893553eb8
Sergei Liddell <liddell.s@pm.me>
2021-05-09 20:06:21 -0600

    Implements random roll functionality for dice

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Die from './Die';
 
 export class RollDice extends Component {
+  static defaultProps = {
+    number: ['one', 'two', 'three', 'four', 'five', 'six']
+  };
   constructor(props) {
     super(props)
 
@@ -9,13 +12,19 @@ export class RollDice extends Component {
       die1: 'one',
       die2: 'one',
     }
+    this.roll = this.roll.bind(this);
   }
-
+  roll() {
+    const newDie1 = this.props.number[Math.floor(Math.random() * this.props.number.length)];
+    const newDie2 = this.props.number[Math.floor(Math.random() * this.props.number.length)];
+    this.setState({die1: newDie1, die2: newDie2})
+  }
   render() {
     return (
       <div>
-        <Die number={this.state.die1}/>
-        <Die number={this.state.die2}/>
+        <Die number={this.state.die1} />
+        <Die number={this.state.die2} />
+        <button onClick={this.roll}>RollDice</button>
       </div>
     )
   }
